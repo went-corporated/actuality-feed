b020a4a5281d400c87595c9e3371e813d5079e65
eKonnect <ekonn63@outlook.fr>
2021-07-23 03:13:04 +0200

    First

---
+++ sidebar/sidebar.js
@@ -0,0 +1,22 @@
+// var f = document.getElementById('foo');
+// document.addEventListener('click', function(ev){
+//     f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
+//     f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
+// },false);
+
+// var helpOption = document.getElementById('help-ui-one').addEventListener("mouseover", mouseOver);
+// var helpOption = document.getElementById('help-ui-one').addEventListener("mouseout", mouseOut);
+
+// function mouseOver() {
+//     document.getElementById("bs-one").style.animationDuration = "5s";
+//     document.getElementById("bs-one").style.filter = "blur(1.5px)";
+//     document.getElementById("ls-one").style.animationDuration = "5s";
+//     document.getElementById("ls-one").style.filter = "blur(1.5px)";
+// }
+
+// function mouseOut() {
+//     document.getElementById("bs-one").style.animationDuration = "18s";
+//     document.getElementById("bs-one").style.filter = "blur(0px)";
+//     document.getElementById("ls-one").style.animationDuration = "18s";
+//     document.getElementById("ls-one").style.filter = "blur(0px)";
+// }

6b62876585c707ac275a9bf94330427c7eaf93dc
eKonnect <ekonn63@outlook.fr>
2021-07-27 00:15:23 +0200

    Popup and transition system

---
+++ popup/popup.js
@@ -0,0 +1,13 @@
+console.log("popup.js is connected.");
+
+function randomize(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+function loaderEnding(){
+    document.getElementById('loader_alpha').classList.toggle('under-transition-fade');
+    document.getElementById('home').classList.toggle('open-transition-slide');
+}
+
+let loadingTime = randomize(300, 1000);
+setTimeout(() => { loaderEnding(); }, loadingTime);

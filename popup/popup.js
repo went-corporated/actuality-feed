60880ca172cd1f143c9ddd83ffa149e8edf963da
eKonnect <ekonn63@outlook.fr>
2021-07-27 06:04:21 +0200

    OMG LE POPUP FONCTIONNE

--- popup/popup.js
+++ popup/popup.js
@@ -11,12 +11,21 @@ document.getElementById('ui_controller').oncontextmenu = function(){
 function loaderEnding(){
     document.getElementById('loader_alpha').classList.toggle('under-transition-fade');
     document.getElementById('home').classList.toggle('open-transition-slide');
+    document.getElementById('loader_frame').style = 'transform: translateX(-20%); transition: .5s;';
 }
 
 document.getElementById('btn_news001').onclick = function(){
     document.getElementById('home_alpha').classList.toggle('zIndexHomeAlpha');
     document.getElementById('home_alpha').classList.toggle('under-transition-fade');
     document.getElementById('news001').classList.toggle('open-transition-slide');
+    document.getElementById('home').style = 'transform: translateX(-120%); transition: .5s;';
 }
 
-setTimeout(() => { loaderEnding(); }, 2000);
+document.getElementById('btn_back_news001').onclick = function(){
+    document.getElementById('home_alpha').classList.toggle('zIndexHomeAlpha');
+    document.getElementById('home_alpha').classList.toggle('under-transition-fade');
+    document.getElementById('news001').classList.toggle('open-transition-slide');
+    document.getElementById('home').style = 'transform: translateX(-100%); transition: .5s;';
+}
+
+setTimeout(() => { loaderEnding(); }, 2500);

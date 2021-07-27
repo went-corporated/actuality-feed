cefb3ef725be2124a6c1056c454c524b0648214b
eKonnect <ekonn63@outlook.fr>
2021-07-27 04:42:04 +0200

    Home

--- popup/popup.js
+++ popup/popup.js
@@ -4,10 +4,19 @@ function randomize(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+document.getElementById('ui_controller').oncontextmenu = function(){
+    return false;
+}
+
 function loaderEnding(){
     document.getElementById('loader_alpha').classList.toggle('under-transition-fade');
     document.getElementById('home').classList.toggle('open-transition-slide');
 }
 
-let loadingTime = randomize(300, 750);
-setTimeout(() => { loaderEnding(); }, loadingTime);
+document.getElementById('btn_news001').onclick = function(){
+    document.getElementById('home_alpha').classList.toggle('zIndexHomeAlpha');
+    document.getElementById('home_alpha').classList.toggle('under-transition-fade');
+    document.getElementById('news001').classList.toggle('open-transition-slide');
+}
+
+setTimeout(() => { loaderEnding(); }, 2000);

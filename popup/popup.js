49040dcbf55cde9c697cf78872c6ba490c9f7b01
eKonnect <ekonn63@outlook.fr>
2021-08-01 03:00:53 +0200

    Contextual menu

--- popup/popup.js
+++ popup/popup.js
@@ -5,7 +5,13 @@ function randomize(min, max) {
 }
 
 document.getElementById('ui_controller').oncontextmenu = function(){
+    contextualMenu();
     return false;
+}
+
+function contextualMenu(){
+    document.getElementById('contextual_menu').classList.toggle('contextual-menu-open');
+    document.getElementById('contextual_menu_alpha').classList.toggle('contextual-menu-alpha-open');
 }
 
 function loaderEnding(){

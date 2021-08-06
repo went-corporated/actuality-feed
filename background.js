ade308c4f38f02b428f77073188892fa8b4d506f
eKonnect <ekonn63@outlook.fr>
2021-08-06 03:40:42 +0200

    bg.js

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 console.log("background.js is connected.");
-
 // chrome.scripting.insertCSS({
 //     target: { tabId: tabs.id },
 //     files: ["inject.css"]
 // });
+
+// chrome.browserAction.setBadgeText({text: 'ON'});
+// chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
